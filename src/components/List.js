f63c71ba5e255cf0370e7c481913b6a2ae4c1fac
Orivelton <orivelton@Oriveltons-MBP.Home>
2020-04-25 22:59:07 +0100

    refactor: created itemList

--- src/components/List.js
+++ src/components/List.js
@@ -4,6 +4,7 @@ import config from '../consts/config';
 import { formatData } from '../ultils/helpers';
 import Search from './Search';
 import Pagination from './Pagination';
+import ListItem from './ListItem';
 
 const List = () => {
   const [list, setList] = useState([]);
@@ -42,41 +43,10 @@ const List = () => {
     <>
       <p>{searchTerm}</p>  
       <Search {...propsSearch} />
+
       <ul>
         {
-          list.map((
-            { name, id, description, categories, subscriptions }
-          ) => (
-            <li key={id}>
-              <div className="app-item">
-                <div className="box-info">
-                  <div className="box-info--content">
-                    <div className="description">
-                      <h1>{name}</h1>
-                      <p>{description}</p>
-                    </div>
-                    <div className="tags">
-                      { categories.map(item => <span key={item}>{item} {'/'}</span>) }
-                    </div>
-                  </div>
-                  <div className="box-info--footer">
-                    <ul>
-                      {
-                        subscriptions.map(({name, price}) => 
-                          <li key={name}>
-                            <span>{name}</span>
-                            <h3>
-                              {price}<sup>€</sup>
-                            </h3>
-                          </li>
-                        )
-                      }
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </li>
-          ))
+          list.map((item) => <ListItem key={item.id} {...item}/>)
         }
       </ul>
 
