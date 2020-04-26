502c0180d10698457c8a25441aa39e9422c1a20d
Orivelton <orivelton@Oriveltons-MBP.Home>
2020-04-26 18:16:01 +0100

    Updates

---
+++ precache-manifest.42c7c37de03902f78e47cd29ecf39e5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2642a052ae960bac174f339d9f4800c6",
+    "url": "/rnters-calendar/index.html"
+  },
+  {
+    "revision": "e8bda91c91ef05797af6",
+    "url": "/rnters-calendar/static/css/main.9789ebc0.chunk.css"
+  },
+  {
+    "revision": "e55acc0961b1869b9c0c",
+    "url": "/rnters-calendar/static/js/2.9a56bd10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rnters-calendar/static/js/2.9a56bd10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8bda91c91ef05797af6",
+    "url": "/rnters-calendar/static/js/main.66b2c656.chunk.js"
+  },
+  {
+    "revision": "1d9527ad7a37f1fab10f",
+    "url": "/rnters-calendar/static/js/runtime-main.7602e8cc.js"
+  }
+]);
